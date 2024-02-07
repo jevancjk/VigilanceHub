7ab294c620c0fc3ede18b61d8328b1acc7962450
jevancjk <2101144@sit.singaporetech.edu.sg>
2024-02-07 10:01:14 +0800

    Include pysimplegui (tentative)

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print("Welcome to VigilanceHub!")
-input("Press Enter to exit")
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Welcome to VigilanceHub!', size=(20, 1), font=('Arial', 20), justification='center')],
+    [sg.Button('Exit', button_color='red')]
+]
+
+window = sg.Window('VigilanceHub', layout, resizable=True, element_justification='center')
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+window.close()
