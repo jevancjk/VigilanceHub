3d82c05760b809db34bc02463bdc98a7f86ef94c
jevancjk <2101144@sit.singaporetech.edu.sg>
2024-05-07 13:08:15 +0800

    Update vigilanceHub.py
    To run the html file through port 5500 instead of the vigilanceHub folder

---
+++ vigilanceHub.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# this is the first page when users load the program
+html_path = '127.0.0.1:5500/index_home.html'
+
+# open the URL indicated in html_path in Google Chrome
+subprocess.Popen(['C:\Program Files\Google\Chrome\Application\chrome.exe', html_path])
