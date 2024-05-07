0470ee5b14ef6f8c645cb3bd908cedf7fa59482a
jevancjk <2101144@sit.singaporetech.edu.sg>
2024-05-07 13:32:14 +0800

    Update vigilanceHub.py

--- vigilanceHub.py
+++ vigilanceHub.py
@@ -4,4 +4,4 @@ import subprocess
 html_path = '127.0.0.1:5500/index_home.html'
 
 # open the URL indicated in html_path in Google Chrome
-subprocess.Popen(['C:\Program Files\Google\Chrome\Application\chrome.exe', html_path])
+subprocess.Popen(['C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', html_path])
