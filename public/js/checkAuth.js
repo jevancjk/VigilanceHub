07ea863ab64cb8db9b7093d27cebcfc470f8f9bb
jevancjk <2101144@sit.singaporetech.edu.sg>
2024-10-25 22:14:32 +0800

    Update 25 Oct

--- public/js/checkAuth.js
+++ public/js/checkAuth.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const authPage = window.location.href;
-    
-    // Fetch a protected resource to check if the user is authenticated
     axios.get('http://127.0.0.1:3000/checkAuth', { withCredentials: true })
         .then(response => {
             console.log('Response data:', response.data);
             console.log('Response status:', response.status);
             if (response.status === 200 && response.data.authenticated) {
-                // window.location.href = '../auth_pages/index_userLogin.html';
                 console.log('User is authenticated');
             } else {
                 throw new Error('Unauthorized');
@@ -15,6 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch(error => {
             console.error('Error checking authentication:', error);
-            window.location.href = '../../misc_html/401.html';
+            window.location.href = '/misc_html/401.html';
         });
     });
