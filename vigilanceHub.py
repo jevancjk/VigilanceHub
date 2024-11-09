cf3270a017b645f54a900318fd66c0323c0e6092
jevancjk <2101144@sit.singaporetech.edu.sg>
2024-11-10 02:45:27 +0800

    Update 10 Nov

--- vigilanceHub.py
+++ vigilanceHub.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # this is the first page when users load the program
-html_path = 'http://127.0.0.1:5500/public/html/index_home.html'
+html_path = 'http://127.0.0.1:5500/public/index.html'
 
 # open the URL indicated in html_path in Google Chrome
 # Change the file path if your path to Google Chrome is different
